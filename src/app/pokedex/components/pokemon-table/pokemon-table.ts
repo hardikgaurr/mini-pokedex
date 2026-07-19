033dbddc267705dff8a9571943c56720751cd541
Hardik Gaur <hardikgaur446@gmail.com>
2026-07-19 17:59:50 +0530

    feat(pokedex): implement material pokemon table

--- src/app/pokedex/components/pokemon-table/pokemon-table.ts
+++ src/app/pokedex/components/pokemon-table/pokemon-table.ts
@@ -1,14 +1,18 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { Pokemon } from '../../../shared/models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-table',
   standalone: true,
+  imports: [MatTableModule],
   templateUrl: './pokemon-table.html',
   styleUrl: './pokemon-table.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonTable {
   readonly pokemons = input.required<Pokemon[]>();
+
+  readonly displayedColumns = ['sprite', 'name', 'types'];
 }
