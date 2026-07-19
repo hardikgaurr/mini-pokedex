f39de508d820e01875835a3a102ff0e6a23d4d14
Hardik Gaur <hardikgaur446@gmail.com>
2026-07-20 00:07:58 +0530

    feat(table): add material table sorting

--- src/app/pokedex/components/pokemon-table/pokemon-table.ts
+++ src/app/pokedex/components/pokemon-table/pokemon-table.ts
@@ -1,8 +1,10 @@
-import { Component, inject, input } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 import { Pokemon } from '../../../shared/models/pokemon.model';
 import { PokemonStore } from '../../state/pokemon.store';
@@ -10,16 +12,29 @@ import { PokemonStore } from '../../state/pokemon.store';
 @Component({
   selector: 'app-pokemon-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatInputModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatTableModule, MatSortModule, MatInputModule],
   templateUrl: './pokemon-table.html',
   styleUrl: './pokemon-table.scss',
 })
-export class PokemonTable {
+export class PokemonTable implements AfterViewInit {
   readonly pokemons = input.required<Pokemon[]>();
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
 
   readonly displayedColumns = ['sprite', 'name', 'types'];
 
+  readonly dataSource = new MatTableDataSource<Pokemon>();
+
   private readonly store = inject(PokemonStore);
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  ngDoCheck(): void {
+    this.dataSource.data = this.pokemons();
+  }
 
   onSearch(value: string): void {
     this.store.setSearchTerm(value);
