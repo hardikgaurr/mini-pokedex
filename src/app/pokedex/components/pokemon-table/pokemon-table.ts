a67222b9f5982e433b702b49f96b37e56eaab5c2
Hardik Gaur <hardikgaur446@gmail.com>
2026-07-20 00:10:13 +0530

    feat(table): add material pagination

--- src/app/pokedex/components/pokemon-table/pokemon-table.ts
+++ src/app/pokedex/components/pokemon-table/pokemon-table.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ViewChild, inject, input } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, ViewChild, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { Pokemon } from '../../../shared/models/pokemon.model';
 import { PokemonStore } from '../../state/pokemon.store';
@@ -12,15 +13,19 @@ import { PokemonStore } from '../../state/pokemon.store';
 @Component({
   selector: 'app-pokemon-table',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatTableModule, MatSortModule, MatInputModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+  ],
   templateUrl: './pokemon-table.html',
   styleUrl: './pokemon-table.scss',
 })
-export class PokemonTable implements AfterViewInit {
+export class PokemonTable implements AfterViewInit, DoCheck {
   readonly pokemons = input.required<Pokemon[]>();
-
-  @ViewChild(MatSort)
-  sort!: MatSort;
 
   readonly displayedColumns = ['sprite', 'name', 'types'];
 
@@ -28,8 +33,15 @@ export class PokemonTable implements AfterViewInit {
 
   private readonly store = inject(PokemonStore);
 
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   ngDoCheck(): void {
