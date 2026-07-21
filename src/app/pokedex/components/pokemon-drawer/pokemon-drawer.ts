870f5095c9dcacfa169c02caa821bdb1dc88ad5c
Hardik Gaur <hardikgaur446@gmail.com>
2026-07-21 15:55:10 +0530

    fix: add drawer close output event

--- src/app/pokedex/components/pokemon-drawer/pokemon-drawer.ts
+++ src/app/pokedex/components/pokemon-drawer/pokemon-drawer.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { Pokemon } from '../../../shared/models/pokemon.model';
 import { PokemonRadarChart } from '../pokemon-radar-chart/pokemon-radar-chart';
@@ -11,13 +12,19 @@ import { PokemonRadarChart } from '../pokemon-radar-chart/pokemon-radar-chart';
 @Component({
   selector: 'app-pokemon-drawer',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatChipsModule, MatIconModule, PokemonRadarChart],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule,
+    PokemonRadarChart,
+  ],
   templateUrl: './pokemon-drawer.html',
   styleUrl: './pokemon-drawer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonDrawer {
   readonly pokemon = input<Pokemon | null>(null);
-
   readonly close = output<void>();
 }
